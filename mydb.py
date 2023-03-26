ce763dcb4e79f8db72d15e35af9c0a47bc08b84a
Tamer Joma <prisoft@live.com>
2023-03-26 15:45:28 +0300

    intial Commit

---
+++ mydb.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'password123',
+	)
+cursorObject = dataBase.cursor()
+cursorObject.execute("CREATE DATABASE elderco")
+print("ALL DONE !")
